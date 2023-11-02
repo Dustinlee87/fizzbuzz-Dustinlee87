cf1bcf7138885e9023f005762f4d94eb3d308f38
Dustinlee87 <122577752+Dustinlee87@users.noreply.github.com>
2023-11-02 18:51:59 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,11 @@
 for num in range(1,101):
     if num % 3 == 0:
         print("fizz")
-    if num % 5 == 0:
+    elif num % 5 == 0:
         print("buzz")
-    if num % 5 == 0 and num % 3 == 0:
+    elif num % 5 == 0 and num % 3 == 0:
         print("fizzbuzz")
-    if num % 5 != 0 and num % 3 != 0:
+    else num % 5 != 0 and num % 3 != 0:
         print(num)
 
 
